3fea5232dc0058effc21f4afcaeab65161231b43
emanresu-username <chenyi.low@gmail.com>
2024-11-26 02:43:46 +0000

    Updates

--- fynesse/assess.py
+++ fynesse/assess.py
@@ -12,6 +12,7 @@ import sklearn.feature_extraction"""
 
 """Place commands in this file to assess the data you have downloaded. How are missing values encoded, how are outliers encoded? What do columns represent, makes rure they are correctly labeled. How is the data indexed. Crete visualisation routines to assess the data (e.g. in bokeh). Ensure that date formats are correct and correctly timezoned."""
 
+import osmnx as ox
 
 def data():
     """Load the data from access and ensure missing values are correctly encoded as well as indices correct, column names informative, date and times correctly formatted. Return a structured data structure such as a data frame."""
@@ -29,3 +30,11 @@ def view(data):
 def labelled(data):
     """Provide a labelled set of data ready for supervised learning."""
     raise NotImplementedError
+
+def pois_from_coordinates(longitude, latitude, tags, box_width=0.02, box_height=0.02):
+    north = latitude + box_height/2
+    south = latitude - box_width/2
+    west = longitude - box_width/2
+    east = longitude + box_width/2
+    pois = ox.features_from_bbox((west, south, east, north), tags)
+    return pois
