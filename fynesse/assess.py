570372e1079cdf7416ba4715c34229ad565ddce0
emanresu-username <chenyi.low@gmail.com>
2024-12-14 08:04:23 +0100

    Added count_pois_near_coordinates

--- fynesse/assess.py
+++ fynesse/assess.py
@@ -48,6 +48,30 @@ def pois_from_coordinates(latitude, longitude, tags, box_width=0.02, box_height=
     pois = ox.features_from_bbox((west, south, east, north), tags)
     return pois
 
+def count_pois_near_coordinates(latitude: float, longitude: float, tags: dict, distance_km: float = 1.0) -> dict:
+    """
+    Count Points of Interest (POIs) near a given pair of coordinates within a specified distance.
+    Args:
+        latitude (float): Latitude of the location.
+        longitude (float): Longitude of the location.
+        tags (dict): A dictionary of OSM tags to filter the POIs (e.g., {'amenity': True, 'tourism': True}).
+        distance_km (float): The distance around the location in kilometers. Default is 1 km.
+    Returns:
+        dict: A dictionary where keys are the OSM tags and values are the counts of POIs for each tag.
+    """
+    poi_counts = {}
+    df = ox.features_from_point(center_point=(latitude,longitude), tags=tags, dist=distance_km*1000) # returns all pois in a circle with radius = distance_km in metres centered at center_point
+    for feature in tags:
+        if feature in df.columns:
+            if tags[feature] == True:
+                poi_counts[feature] = df[feature].notnull().sum()
+            elif type(tags[feature]) == list:
+                poi_counts[feature] = df[feature].isin(tags[feature]).sum()
+        else:
+            poi_counts[feature] = 0
+
+    return poi_counts
+
 def add_feature_geometry(df: pd.DataFrame):
     wgs84 = CRS.from_epsg(4326)
     bng = CRS.from_epsg(27700)
